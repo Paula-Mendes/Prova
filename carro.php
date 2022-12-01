e6fef6025364a6d34d15c092b48ba9336e17b301
Paula Mendes <paulapassosmendes.discente@fha.mg.gov.br>
2022-12-01 19:03:51 -0300

    Prova 01/12/22

---
+++ carro.php
@@ -0,0 +1,39 @@
+<?php
+
+class Carro
+{
+    public $marca = "Fiat";
+    public $modelo = "Uno Mille Way";
+    public $anoFabricacao = 2011;
+    public $quantPortas = 4;
+    public $cor = "Vermelho";
+
+    public function caracteristicas()
+    {
+        echo "Marca: {$this -> marca} <br>";
+        echo "Modelo: {$this -> modelo} <br>";
+        echo "Fabricação: {$this -> anoFabricacao} <br>";
+        echo "Qauntidade de portas: {$this -> quantPortas} <br>";
+        echo "Cor: {$this -> cor} <br>";
+        echo "<br>";
+    }
+
+    public function andar()
+    {
+        echo "Carro andando!";
+        echo "<br>";
+    }
+
+    public function fazerBarulho()
+    {
+        echo "<br>";
+        echo "Vruuuuuum!";
+       
+    }
+
+}
+
+$carro1 = new Carro();
+$carro1 -> caracteristicas();
+$carro1 -> andar();
+$carro1 -> fazerBarulho();
---
+++ escola.php
@@ -0,0 +1,38 @@
+<?php
+
+class Escola
+{
+    public $nome = "Escola Técnica Sandoval Soares de Azevedo";
+    public $cursos = "Informática, RH, Administração, Enfermagem";
+    public $quantTurmas = 10;
+    public $quantProfessores = 10;
+    public $endereco = "R. Quarenta e Dois, 1-283 - Campos Filho, Ibirité - MG";
+
+    public function caracteristicas()
+    {
+        echo "Nome: {$this -> nome} <br>";
+        echo "Cursos: {$this -> cursos} <br>";
+        echo "Quantidade turmas: {$this -> quantTurmas} <br>";
+        echo "Quantidade de professores: {$this -> quantProfessores} <br>";
+        echo "Endereço: {$this -> endereco} <br>";
+    }
+
+    public function ensinar()
+    {
+        echo "<br>";
+        echo "O dever da escola é ensinar!";
+        echo "<br>";
+    }
+
+    public function profissionalizar()
+    {
+        echo "<br>";
+        echo "A escola deve preparar o aluno para o mercado de trabalho.";
+    }
+
+}
+
+$escola1 = new Escola();
+    $escola1 -> caracteristicas();
+    $escola1 -> ensinar();
+    $escola1 -> profissionalizar();
